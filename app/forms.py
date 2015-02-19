4e4c37a79aa93486495c6d3614ed545464565c7d
Anastasia Nishnevich <anastasia.nishnevich@gmail.com>
2015-02-18 18:28:05 -0800

    Added filters and default value to distance

--- app/forms.py
+++ app/forms.py
@@ -9,11 +9,15 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import ugettext_lazy as _
 
 class SearchForm(forms.Form):
-    query = forms.CharField(label='Query', max_length=200, required=False)
-    distance = forms.FloatField(label='Distance', max_value=200, required=False)
-    title = forms.CharField(label='Title', max_length=200, widget=forms.TextInput(attrs={"onkeyup" : "searchOpen('title')"}), required=False)
-    year = forms.IntegerField(label='Year', max_value=2020, min_value=1880, required=False, error_messages = {'min_value': "Cinematography was only invented in 1880's."})
+    query              = forms.CharField(label='Query', max_length=200, required=False)
+    distance           = forms.FloatField(label='Distance', max_value=200, required=False, initial=3)
+    title              = forms.CharField(label='Title', max_length=200, widget=forms.TextInput(attrs={"onkeyup" : "searchOpen('title')"}), required=False)
+    year               = forms.IntegerField(label='Year', max_value=2020, min_value=1880, required=False, error_messages = {'min_value': "Cinematography was only invented in 1880's."})
     production_company = forms.CharField(label='Production Company', max_length=200, widget=forms.TextInput(attrs={"onkeyup" : "searchOpen('production_company')"}), required=False)
+    distributor        = forms.CharField(label='Distributor', max_length=200, widget=forms.TextInput(attrs={"onkeyup" : "searchOpen('distributor')"}), required=False)
+    director           = forms.CharField(label='Director', max_length=200, widget=forms.TextInput(attrs={"onkeyup" : "searchOpen('director')"}), required=False)
+    writer             = forms.CharField(label='Writer', max_length=200, widget=forms.TextInput(attrs={"onkeyup" : "searchOpen('writer')"}), required=False)
+    actor              = forms.CharField(label='Actor', max_length=200, widget=forms.TextInput(attrs={"onkeyup" : "searchOpen('actor')"}), required=False)
 
     def clean(self):
         is_address_filled = self.cleaned_data.get('query', False)
